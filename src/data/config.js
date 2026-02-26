042a9b53d333958ff7090d085c04e42dd736046f
Felipe França <gilgameshlighthan@gmail.com>
2026-02-26 09:12:46 -0300

    suporte

--- src/data/config.js
+++ src/data/config.js
@@ -3,7 +3,7 @@ const brand = {
   name: "Cubo Criativo",
   slogan: "Miniaturas em resina • Pintura artística • Modelagem 3D",
   whatsapp: "5577998211169",
-  email: "cubocriativox0@gmail.com",
+  email: "cubocriativosuporte@gmail.com",
   insta: "@_cubocriativo_",
   city: "Barreiras - BA",
   logo: "/images/logo.png",
